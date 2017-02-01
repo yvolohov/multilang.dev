c1968618a705e5fdfb14068c2c72952d634d6d52
Yaroslav Volohov <yvolohov@gmail.com>
2017-02-01 23:09:22 +0200

    upper and lower case fields

--- sql/schema.sql
+++ sql/schema.sql
@@ -3,6 +3,8 @@ CREATE TABLE IF NOT EXISTS `words_en` (
   `chapter_id` INT UNSIGNED NOT NULL,
   `word` VARCHAR(100) NOT NULL DEFAULT '',
   `words_count` INT UNSIGNED NOT NULL DEFAULT 0,
+  `uppercase` TINYINT UNSIGNED NOT NULL DEFAULT 0,
+  `lowercase` TINYINT UNSIGNED NOT NULL DEFAULT 0,
   PRIMARY KEY (`book_id`, `chapter_id`, `word`)
 ) ENGINE = InnoDB;
 
@@ -11,5 +13,7 @@ CREATE TABLE IF NOT EXISTS `words_ru` (
   `chapter_id` INT UNSIGNED NOT NULL,
   `word` VARCHAR(100) NOT NULL DEFAULT '',
   `words_count` INT UNSIGNED NOT NULL DEFAULT 0,
+  `uppercase` TINYINT UNSIGNED NOT NULL DEFAULT 0,
+  `lowercase` TINYINT UNSIGNED NOT NULL DEFAULT 0,
   PRIMARY KEY (`book_id`, `chapter_id`, `word`)
 ) ENGINE = InnoDB;
